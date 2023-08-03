97e0c0ee17b329a71fe6592d1714b1ec480cf085
Chad Baker <chad.baker@nrel.gov>
2023-08-03 15:09:22 -0600

    rounding works for big numbers but fails badly for small numbers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("{}\n", format_f64_eng(std::f64::consts::PI, None));
-    println!("{}\n", format_f64_eng(33.333, None));
+    println!("{}\n", format_f64_eng(33.333, Some(7)));
     println!("{}\n", format_f64_eng(66.666, None));
     println!("{}\n", format_f64_eng(333.33, None));
     println!("{}\n", format_f64_eng(666.66, None));
@@ -23,16 +23,22 @@ fn format_f64_eng(x: f64, s: Option<usize>) -> String {
     let exp: usize = x.abs().log10().floor() as usize - x.abs().log10().floor() as usize % 3;
     println!("exp: {exp}");
 
+    // number of digits left of decimal
     let n_left_of_dec: usize = x.abs().log10().floor() as usize % 3 + 1;
     println!("n_left_of_dec: {}", n_left_of_dec);
 
-    match x {
+    // number of digits right of decimal
+    let n_dec = s - n_left_of_dec;
+
+    let x_rounded = (x * n_dec as f64).round() / n_dec as f64;
+
+    match x_rounded {
         x if exp < 3 => {
-            format!("{x:.*}", s - n_left_of_dec)
+            format!("{x:.*}", n_dec)
         }
         x => {
             let x = x / 10_f64.powf(exp as f64);
-            format!("{x:.*}e{exp}", s - n_left_of_dec)
+            format!("{x:.*}e{exp}", n_dec)
         }
     }
 }
