b33071780581412eeeb51a2354a7c58789e91d61
Chad Baker <chad.baker@nrel.gov>
2023-08-04 13:45:17 -0600

    changed to trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,106 @@
-//! Module containing function (and maybe trait?) to format f64 in [engineering
+//! Module containing trait to format f64 in [engineering
 //! notation](https://en.wikipedia.org/wiki/Engineering_notation)
 
-/// Returns f64 as string in [engineering
-/// notation](https://en.wikipedia.org/wiki/Engineering_notation) with last digit rounded to nearest
-/// rather than truncated.
-/// # Arguments
-/// * x - value to be formatted
-/// * s - number of significant figures, defaults to 3
-/// # Examples
-/// ```
-/// fn test_pi() {
-///     assert_eq!(
-///         eng_fmt::format_f64_eng(std::f64::consts::PI, None),
-///         Ok(String::from("3.14"))
-///     );
-/// }
-/// ```
-/// ```
-/// fn test_pi_5d() {
-///     assert_eq!(
-///         eng_fmt::format_f64_eng(std::f64::consts::PI, Some(5)),
-///         Ok(String::from("3.1415"))
-///     );
-/// }
-/// ```
-/// ```
-/// fn test_2pi_5d() {
-///     assert_eq!(
-///         eng_fmt::format_f64_eng(std::f64::consts::PI * 2., Some(5)),
-///         Ok(String::from("3.1415"))
-///     );
-/// }
-/// ```
-/// ```
-/// fn test_err() {
-///     assert_eq!(
-///         eng_fmt::format_f64_eng(std::f64::consts::PI, Some(2)),
-///         Err("Number of significant figures `s` cannot be less than 3!".to_string())
-///     );
-/// }
-/// ```
-pub fn format_f64_eng(x: f64, s: Option<usize>) -> Result<String, String> {
-    let s = s.unwrap_or(3);
-    if s < 3 {
-        return Err("Number of significant figures `s` cannot be less than 3!".to_string());
-    }
+/// Trait providing method for formatting numbers in [engineering
+/// notation](https://en.wikipedia.org/wiki/Engineering_notation)
+pub trait FormatEng {
+    fn format_eng(&self, sf: Option<usize>) -> Result<String, String>;
+}
 
-    // engineering notation exponent
-    let exp_eng: usize = x.abs().log10().floor() as usize - x.abs().log10().floor() as usize % 3;
-
-    let exp_sci = x.abs().log10().floor() as usize;
-
-    // number of digits left of decimal _after_ formatting for engineering notation, should never
-    // exceed 3
-    let n_left_of_dec: usize = x.abs().log10().floor() as usize % 3 + 1;
-
-    assert!(
-        n_left_of_dec <= 3,
-        "n_left_of_dec: {} exceeds 3",
-        n_left_of_dec
-    );
-
-    let n_dec = s - n_left_of_dec;
-
-    let mut x_base = match exp_eng {
-        _ if exp_eng <= 2 => x,
-        _ => x / 10_f64.powi(exp_eng as i32),
-    };
-
-    x_base = match n_left_of_dec {
-        // the other branch should work for this first case, but doing this separately should be
-        // more cpu efficient
-        _ if n_left_of_dec == 3 => x_base.round(),
-        _ => {
-            (x_base * 10_f64.powi((s - n_left_of_dec) as i32)).round()
-                * 10_f64.powf(-((s - n_left_of_dec) as f64))
+impl FormatEng for f64 {
+    /// Returns f64 as string in [engineering
+    /// notation](https://en.wikipedia.org/wiki/Engineering_notation) with last digit rounded to nearest
+    /// rather than truncated.
+    /// # Arguments
+    /// * x - value to be formatted
+    /// * s - number of significant figures, defaults to 3
+    /// # Examples
+    /// ```
+    /// fn test_one() {
+    ///     use eng_fmt::FormatEng;
+    ///     assert_eq!(
+    ///         1_f64.format_eng(None),
+    ///         Ok(String::from("001"))
+    ///     );
+    /// }
+    /// ```
+    /// ```
+    /// fn test_pi() {
+    ///     use eng_fmt::FormatEng;
+    ///     assert_eq!(
+    ///         std::f64::consts::PI.format_eng(None),
+    ///         Ok(String::from("3.14"))
+    ///     );
+    /// }
+    /// ```
+    /// ```
+    /// fn test_pi_5d() {
+    ///     use eng_fmt::FormatEng;
+    ///     assert_eq!(
+    ///         std::f64::consts::PI.format_eng(Some(5)),
+    ///         Ok(String::from("3.1415"))
+    ///     );
+    /// }
+    /// ```
+    /// ```
+    /// fn test_2pi_5d() {
+    ///     use eng_fmt::FormatEng;
+    ///     assert_eq!(
+    ///         (std::f64::consts::PI * 2.).format_eng(Some(5)),
+    ///         Ok(String::from("3.1415"))
+    ///     );
+    /// }
+    /// ```
+    /// ```
+    /// fn test_err() {
+    ///     use eng_fmt::FormatEng;
+    ///     assert_eq!(
+    ///         std::f64::consts::PI.format_eng(Some(2)),
+    ///         Err("Number of significant figures `s` cannot be less than 3!".to_string())
+    ///     );
+    /// }
+    /// ```
+    fn format_eng(&self, sf: Option<usize>) -> Result<String, String> {
+        let s = sf.unwrap_or(3);
+        if s < 3 {
+            return Err("Number of significant figures `s` cannot be less than 3!".to_string());
         }
-    };
 
-    match exp_eng {
-        _ if exp_eng <= 2 => Ok(format!("{x_base:.*}", n_dec)),
-        _ => Ok(format!("{x_base:.*}e{}", n_dec, exp_eng)),
+        // engineering notation exponent
+        let exp_eng: usize =
+            self.abs().log10().floor() as usize - self.abs().log10().floor() as usize % 3;
+
+        // number of digits left of decimal _after_ formatting for engineering notation, should never
+        // exceed 3
+        let n_left_of_dec: usize = self.abs().log10().floor() as usize % 3 + 1;
+
+        assert!(
+            n_left_of_dec <= 3,
+            "n_left_of_dec: {} exceeds 3",
+            n_left_of_dec
+        );
+
+        let n_dec = s - n_left_of_dec;
+
+        let mut x_base = match exp_eng {
+            _ if exp_eng <= 2 => *self,
+            _ => self / 10_f64.powi(exp_eng as i32),
+        };
+
+        x_base = match n_left_of_dec {
+            // the other branch should work for this first case, but doing this separately should be
+            // more cpu efficient
+            _ if n_left_of_dec == 3 => x_base.round(),
+            _ => {
+                (x_base * 10_f64.powi((s - n_left_of_dec) as i32)).round()
+                    * 10_f64.powf(-((s - n_left_of_dec) as f64))
+            }
+        };
+
+        match exp_eng {
+            _ if exp_eng <= 2 => Ok(format!("{x_base:.*}", n_dec)),
+            _ => Ok(format!("{x_base:.*}e{}", n_dec, exp_eng)),
+        }
     }
 }
 
@@ -90,65 +110,62 @@ mod tests {
     #[test]
     fn test_pi() {
         assert_eq!(
-            format_f64_eng(std::f64::consts::PI, None),
+            std::f64::consts::PI.format_eng(None),
             Ok(String::from("3.14"))
         );
     }
     #[test]
     fn test_33p333() {
-        assert_eq!(
-            format_f64_eng(33.333, Some(7)),
-            Ok(String::from("33.33300"))
-        );
+        assert_eq!(33.333_f64.format_eng(Some(7)), Ok(String::from("33.33300")));
     }
     #[test]
     fn test_66p666() {
-        assert_eq!(format_f64_eng(66.666, None), Ok(String::from("66.7")));
+        assert_eq!(66.666_f64.format_eng(None), Ok(String::from("66.7")));
     }
     #[test]
     fn test_333p33() {
-        assert_eq!(format_f64_eng(333.33, None), Ok(String::from("333")));
+        assert_eq!(333.33_f64.format_eng(None), Ok(String::from("333")));
     }
     #[test]
     fn test_666p66() {
-        assert_eq!(format_f64_eng(666.66, None), Ok(String::from("667")));
+        assert_eq!(666.66_f64.format_eng(None), Ok(String::from("667")));
     }
     #[test]
     fn test_3p3333e3() {
-        assert_eq!(format_f64_eng(3.3333e3, None), Ok(String::from("3.33e3")));
+        assert_eq!(3.3333e3_f64.format_eng(None), Ok(String::from("3.33e3")));
     }
     #[test]
     fn test_6p6666e3() {
-        assert_eq!(format_f64_eng(6.6666e3, None), Ok(String::from("6.67e3")));
+        assert_eq!(6.6666e3_f64.format_eng(None), Ok(String::from("6.67e3")));
     }
     #[test]
     fn test_33p333e6() {
-        assert_eq!(format_f64_eng(33.333e6, None), Ok(String::from("33.3e6")));
+        assert_eq!(33.333e6_f64.format_eng(None), Ok(String::from("33.3e6")));
     }
     #[test]
     fn test_66p666e6() {
-        assert_eq!(format_f64_eng(66.666e6, None), Ok(String::from("66.7e6")));
+        assert_eq!(66.666e6_f64.format_eng(None), Ok(String::from("66.7e6")));
     }
     #[test]
     fn test_2pi_5d() {
         assert_eq!(
-            format_f64_eng(std::f64::consts::PI * 2., Some(5)),
+            (std::f64::consts::PI * 2.).format_eng(Some(5)),
             Ok(String::from("6.2832"))
         );
     }
     #[test]
     fn test_n2pi_5d() {
         assert_eq!(
-            format_f64_eng(-std::f64::consts::PI * 2., Some(5)),
+            (-std::f64::consts::PI * 2.).format_eng(Some(5)),
             Ok(String::from("-6.2832"))
         );
     }
     #[test]
     fn test_zero_5sf() {
-        assert_eq!(format_f64_eng(0., Some(5)), Ok(String::from("0.0000")));
+        assert_eq!(0_f64.format_eng(Some(5)), Ok(String::from("0.0000")));
     }
     #[test]
     fn test_zero() {
-        assert_eq!(format_f64_eng(0., None), Ok(String::from("0.00")));
+        assert_eq!(0_f64.format_eng(None), Ok(String::from("0.00")));
     }
 }
